a5e0c6bad0121754fb705e25539243e4df1fa4e5
jkoldun <jaroslaw.koldun@deployed.pl>
2023-09-30 18:59:10 +0200

    Changed models

--- hackyeah/routes/models.py
+++ hackyeah/routes/models.py
@@ -3,15 +3,38 @@ from pathlib import Path
 from django.contrib.gis.geos import Point
 from django.db import models
 
+from hackyeah.routes.constants import RouteType
 from hackyeah.routes.entities import GeocodedPlace
 
+ROUTE_IMAGE_UPLOAD_TO = Path("route", "images", "%Y", "%m", "%d")
 ROUTE_POINTS_IMAGE_UPLOAD_TO = Path("route_points", "images", "%Y", "%m", "%d")
 ROUTE_POINTS_AUDIO_UPLOAD_TO = Path("route_points", "audio", "%Y", "%m", "%d")
+HERO_IMAGE_UPLOAD_TO = Path("route_points", "audio", "%Y", "%m", "%d")
+PARAGRAPH_IMAGE_UPLOAD_TO = Path("route_points", "audio", "%Y", "%m", "%d")
+
+
+class Hero(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=HERO_IMAGE_UPLOAD_TO, blank=True, null=True)
+
+
+class City(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
 
 
 class Route(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
+    image = models.ImageField(upload_to=ROUTE_IMAGE_UPLOAD_TO, blank=True, null=True)
+    duration = models.TimeField(default="01:00:00")
+    route_type = models.CharField(
+        choices=RouteType.choices, max_length=20, default=RouteType.historical
+    )
+    hero = models.ForeignKey(Hero, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -42,7 +65,9 @@ class BasePoint(models.Model):
 
 
 class RoutePoint(BasePoint):
-    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    route = models.ForeignKey(
+        Route, related_name="route_points", on_delete=models.CASCADE
+    )
     order = models.PositiveSmallIntegerField()
     description = models.TextField(blank=True)
     main_image = models.ImageField(
@@ -57,3 +82,19 @@ class RoutePoint(BasePoint):
 
     def __str__(self):
         return self.name
+
+
+class Paragraph(models.Model):
+    order = models.PositiveSmallIntegerField()
+    route_point = models.ForeignKey(
+        RoutePoint, related_name="paragraphs", on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    image = models.ImageField(upload_to=HERO_IMAGE_UPLOAD_TO, blank=True, null=True)
+    image_description = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = ("route_point", "order")
+
+    def __str__(self):
+        return f"{self.route_point.name} - {self.order}"
--- hackyeah/routes/serializers.py
+++ hackyeah/routes/serializers.py
@@ -4,13 +4,19 @@ from hackyeah.routes.models import RoutePoint, Route
 
 
 class RoutePointSerializer(serializers.ModelSerializer):
+    coordinate = serializers.SerializerMethodField()
+
     class Meta:
         model = RoutePoint
+        fields = ("id", "name", "short_description", "coordinate")
+
+    def get_coordinate(self, obj):
+        return {"latitude": obj.latitude, "longitude": obj.longitude}
 
 
 class RouteSerializer(serializers.ModelSerializer):
-    routepoint_set = RoutePointSerializer(many=True)
+    route_points = RoutePointSerializer(many=True)
 
     class Meta:
         model = Route
-        fields = ("name", "description", "routepoint_set")
+        fields = ("route_points",)
